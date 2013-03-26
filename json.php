96c74e351f4f95d5151653238e4447d59823e53d
Pedro Markun <pedro@esfera.mobi>
2013-03-26 03:09:41 -0300

    bad caching system

--- json.php
+++ json.php
@@ -1,7 +1,20 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header('content-type: application/json; charset=utf-8');
-$url = 'http://catarse.me/pt/projects/' . htmlspecialchars($_GET["project"]) .  '/backers?page=' . htmlspecialchars($_GET["page"]);
-$json = file_get_contents($url);
-echo $json;
+$project = htmlspecialchars($_GET["project"]);
+$page = htmlspecialchars($_GET["page"]);
+$url = 'http://catarse.me/pt/projects/' . $project .  '/backers?page=' . $page;
+$cache_file = "data/" . $project . "_" . $page . ".json";
+if (file_exists($cache_file) && (filemtime($cache_file) > (time() - 60 * 5 ))) {
+   // Cache file is less than five minutes old. 
+   // Don't bother refreshing, just use the file as-is.
+   $file = file_get_contents($cache_file);
+} else {
+   // Our cache is out-of-date, so load the data from our remote server,
+   // and also save it over our cache for next time.
+   $file = file_get_contents($url);
+   file_put_contents($cache_file, $file, LOCK_EX);
+}
+echo $file;
 ?>
+
